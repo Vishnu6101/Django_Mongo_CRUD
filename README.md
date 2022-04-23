b1b7650269bb47866ba7ec51faa911ecbb9e3a3b
Vishnukumar NV <vishnukumarnv6101@gmail.com>
2022-04-23 12:09:41 +0530

    README Added

---
+++ README.md
@@ -0,0 +1,10 @@
+# Django_Mongo_CURD User Management Application
+
+**This is a simple Django Web Application that uses MongoDB as a backend database and performs CURD operations**
+
+---
+- Technologies Used:
+  1. Django
+  2. MongoDB
+  3. HTML, TailWindCSS, Jinja2 Templates
+---
--- user/forms.py
+++ user/forms.py
@@ -2,10 +2,6 @@ from django import forms
 from .models import User
 
 class Add_A_User(forms.ModelForm):
-    # name = forms.CharField(label='name', max_length=256)
-    # email = forms.EmailField()
-    # gender = forms.CharField(label='gender', max_length=10)
-    # status = forms.CharField(label='status', max_length=20)
     class Meta:
         model = User
         fields = ['name', 'email', 'gender', 'status']
